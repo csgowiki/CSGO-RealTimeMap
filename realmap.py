dd90fda426e94c524066215a5630e4df52bdee89
Herixth <17361019875@163.com>
2020-12-03 14:13:43 +0800

    多玩家，接口已测试

--- realmap.py
+++ realmap.py
@@ -1,12 +1,36 @@
+import os
+import json
 from flask import Flask, render_template, request
 
-import mapconvert
+class Converter_Real2Img():
+    def __init__(self, map_name: str):
+        self.load_map(map_name)
+
+    def load_map(self, map_name: str):
+        try:
+            map_info_path = os.path.join('static', 'json', 'map_info.json')
+            with open(map_info_path, 'r') as infile:
+                self.map_info = json.loads(infile.read())[map_name]
+        except:
+            raise Exception('map_info.json load FAILED!')
+    
+    def convert(self, realX: float, realY: float):
+        try:
+            imgX = '{}px'.format((realX - self.map_info['pos_x']) / self.map_info['scale'])
+            imgY = '{}px'.format((self.map_info['pos_y'] - realY) / self.map_info['scale'])
+            return imgX, imgY
+        except:
+            raise Exception('convert FAILED!')
+
+__CHARSPLIT = '|'
+__NAMESPLIT = '!@!'
+__MAXPLAYERS = 10
 
 app = Flask(__name__)
-
-mp_converter = mapconvert.Converter_Real2Img('de_inferno')
-
-infoContainer = {"cx": 5, "cy": 5}
+mp_converter = Converter_Real2Img('de_inferno')
+infoContainer = {
+    'players': []
+}
 
 @app.route('/')
 def mapview():
@@ -17,15 +41,37 @@ def ajaxview():
     global infoContainer
     return infoContainer
 
+@app.route('/api/ajax_init', methods=["GET"])
+def ajaxInitView():
+    global __MAXPLAYERS
+    return {"maxplayers" : __MAXPLAYERS }
+
 @app.route('/api/server_update', methods=["POST", "GET"])
 def serverview():
     if request.method == "POST":
-        cx, cy = mp_converter.convert(
-            float(request.form.get("realX", 0)),
-            float(request.form.get("realY", 0))
-        )
-        global infoContainer
-        infoContainer = {"cx": cx, "cy": cy}
+        '''
+        playerXs: xx|xx|xx
+        playerYs: xx|xx|xx
+        steam3ids: xx|xx|xx
+        names: xx !@! xx !@! xx
+        '''
+        global infoContainer, __CHARSPLIT, __NAMESPLIT
+        playerXs = request.form.get('playerXs', []).split(__CHARSPLIT)
+        playerYs = request.form.get('playerYs', []).split(__CHARSPLIT)
+        steam3ids = request.form.get('steam3ids', []).split(__CHARSPLIT)
+        names = request.form.get('names', []).split(__NAMESPLIT)
+        # check valid
+        if len(playerXs) != len(names):
+            return {"status": "error", "message": "player's name INVALID"}
+
+        playerCount = len(playerXs)
+        infoContainer["players"].clear()
+        for player in range(playerCount):
+            px, py = mp_converter.convert(float(playerXs[player]), float(playerYs[player]))
+            infoContainer["players"].append({
+                "posX": px, "posY": py,
+                "steam3id": steam3ids[player], "name": names[player]
+            })
         return {"status": "Ok"}
     return {"status": "None", "message": "POST only"}
 
--- static/js/realmap.js
+++ static/js/realmap.js
@@ -1,35 +1,52 @@
-function draw(data) {
-   var test = document.getElementById("test");
-   var new_x = parseFloat(data.cx);
-   var new_y = parseFloat(data.cy);
-   var origin_x = parseFloat(test.getAttribute("cx"));
-   var origin_y = parseFloat(test.getAttribute("cy"));
-   test.setAttribute("cx", (origin_x + (new_x - origin_x) / 50).toString() + "px");
-   test.setAttribute("cy", (origin_y + (new_y - origin_y) / 50).toString() + "px");
+const MAXPLAYERS = 10;
+const UPDATE_INTERVAL = 1000; // ms
+
+function initPlayers(maxplayers = MAXPLAYERS) {
+   for (var player = 0; player < maxplayers; player++) {
+      $("#canvas").append($(
+         `<svg id='playerpos${player}' visibility='hidden' style='position:absolute;top:0px;left:0px;width:100px;height:20px;' xmlns='http://www.w3.org/2000/svg' version='1.1'> \
+            <circle cx='15' cy='5' r='5' stroke='black' fill='red' /> \
+            <a id='playerXlink${player}' href='#'><text id='playername${player}' x='0' y='20' fill='yellow'></text></a> \
+         </svg>`
+      ))
+   }
 }
 
-function drawUtility(data){
-   let start = Date.now();
-   let timer = setInterval(function() {
-      let timePassed = Date.now() - start;
-      if (timePassed >= 1000) {
-         clearInterval(timer);
-         return;
+function drawPlayer(data) {
+   for (var player = 0; player < MAXPLAYERS; player++) {
+      if (player < data.players.length) {
+         $(`#playerpos${player}`).attr('visibility', 'visible');
+         $(`#playername${player}`).text(data.players[player].name);
+         $(`#playerXlink${player}`).attr('href', `https://steamcommunity.com/profiles/${data.players[player].steam3id}`)
+         $(`#playerpos${player}`).animate(
+            { left: data.players[player].posX, top: data.players[player].posY }, 
+            UPDATE_INTERVAL
+         );
       }
-      draw(data)
-   }, 20);
+      else {
+         $(`#playerpos${player}`).attr('visibility', 'hidden');
+      }
+   }
 }
 
 $(function(){
+   $.ajax({
+      async: false,
+      url: "/api/ajax_init",
+      type: "get",
+      success(data) {
+         initPlayers(data.maxplayers)
+      }
+   })
+
    window.setInterval(function() {
       $.ajax({
          async: false,
          url: "/api/ajax_update",
          type: "get",
-
          success(data) {
-            drawUtility(data);
+            drawPlayer(data);
          }
       })
-   }, 1000);
+   }, UPDATE_INTERVAL);
 })
